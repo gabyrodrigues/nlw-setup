5ed58cd2f74837de430b6fe7616b0db6162c1a61
Gabriely <gabriely.lima@nave.rs>
2023-01-18 19:48:58 -0300

    mobile: fill summary days with placeholder habit days block when year has not enough days to fill the whole table

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,10 +1,12 @@
-import { Text, View } from "react-native"
+import { Text, View, ScrollView } from "react-native"
 import { generateDatesFromYearBeginning } from "../../utils/generate-dates-from-year-beginning"
 import { DAY_SIZE, HabitDay } from "../HabitDay"
 import { Header } from "../Header"
 
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"]
 const datesFromYearStart = generateDatesFromYearBeginning()
+const minimumSummaryDatesSize = 18 * 5 //18 weeks
+const amountOfDaysToFill = minimumSummaryDatesSize - datesFromYearStart.length // placeholder to fill HabitsDay when summarDates is not enough days visually
 
 export function Home() {
   return (
@@ -23,11 +25,24 @@ export function Home() {
         ))}
       </View>
 
-      <View className="flex-row flex-wrap">
-        {datesFromYearStart.map((date) => (
-          <HabitDay key={date.toISOString()} />
-        ))}
-      </View>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <View className="flex-row flex-wrap">
+          {datesFromYearStart.map((date) => (
+            <HabitDay key={date.toISOString()} />
+          ))}
+
+          {amountOfDaysToFill > 0 &&
+            Array.from({ length: amountOfDaysToFill }).map((_, index) => (
+              <View
+                className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                style={{ width: DAY_SIZE, height: DAY_SIZE }}
+              />
+            ))}
+        </View>
+      </ScrollView>
     </View>
   )
 }
