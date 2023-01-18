57102aa7c77f27fa26630d6b940a0d7e8d316f18
Gabriely <gabriely.lima@nave.rs>
2023-01-17 22:56:58 -0300

    web: fill summary days with placeholder days when year has not enough days to fill the whole table

--- web/src/components/SummaryTable/index.tsx
+++ web/src/components/SummaryTable/index.tsx
@@ -5,6 +5,9 @@ const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"]
 
 const summaryDates = generateDatesFromYearBeginning()
 
+const minimumSummaryDatesSize = 18 * 7 //18 weeks
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length // placeholder to fill HabitsDay when summarDates is not enough days visually
+
 export function SummaryTable() {
   return (
     <div className="w-full flex">
@@ -23,6 +26,14 @@ export function SummaryTable() {
         {summaryDates.map((date) => (
           <HabitDay key={date.toString()} />
         ))}
+
+        {amountOfDaysToFill > 0 &&
+          Array.from({ length: amountOfDaysToFill }).map((_, index) => (
+            <div
+              key={index}
+              className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+            />
+          ))}
       </div>
     </div>
   )
