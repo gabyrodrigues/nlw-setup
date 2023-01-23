736eeb6d7ec1319726bb44f38fbc52187e10b920
Gabriely <gabriely.lima@nave.rs>
2023-01-23 19:16:54 -0300

    web: creating new habit form checkbox list + function to add or remove weekdays array

--- web/src/components/NewHabitForm/index.tsx
+++ web/src/components/NewHabitForm/index.tsx
@@ -1,8 +1,40 @@
+import { FormEvent, useState } from "react"
+
 import { Check } from "phosphor-react"
+import * as Checkbox from "@radix-ui/react-checkbox"
+
+const availableWeekDays = [
+  "Domingo",
+  "Segunda-feira",
+  "Terça-feira",
+  "Quarta-feira",
+  "Quinta-feira",
+  "Sexta-feira",
+  "Sábado",
+]
 
 export function NewHabitForm() {
+  const [title, setTitle] = useState("")
+  const [weekDays, setWeekDays] = useState<number[]>([])
+
+  function createNewHabit(event: FormEvent) {
+    event.preventDefault()
+
+    console.log("create", title, weekDays)
+  }
+
+  function handleToggleWeekDay(weekDayIndex: number) {
+    if (weekDays.includes(weekDayIndex)) {
+      setWeekDays((prevState) =>
+        prevState.filter((weekDay) => weekDay !== weekDayIndex)
+      )
+    } else {
+      setWeekDays((prevState) => [...prevState, weekDayIndex])
+    }
+  }
+
   return (
-    <form className="w-full flex flex-col mt-6">
+    <form onSubmit={createNewHabit} className="w-full flex flex-col mt-6">
       <label className="font-semibold leading-tight">
         Qual seu comprometimento?
       </label>
@@ -13,11 +45,30 @@ export function NewHabitForm() {
         placeholder="ex.: Exercícios, Dormir bem, etc..."
         className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
         autoFocus
+        onChange={(event) => setTitle(event.target.value)}
       />
 
       <label htmlFor="" className="font-semibold leading-tight mt-4">
         Qual a recorrência?
       </label>
+
+      <div className="flex flex-col gap-2 mt-3">
+        {availableWeekDays.map((weekDay, index) => (
+          <Checkbox.Root
+            key={weekDay}
+            className="flex items-center gap-3 group"
+            onCheckedChange={() => handleToggleWeekDay(index)}
+          >
+            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+              <Checkbox.Indicator>
+                <Check size={20} className="text-white" />
+              </Checkbox.Indicator>
+            </div>
+
+            <span className="text-white leading-tight">{weekDay}</span>
+          </Checkbox.Root>
+        ))}
+      </div>
 
       <button
         type="submit"
