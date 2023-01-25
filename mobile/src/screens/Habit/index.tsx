d2def0c4da80d69851f60ee5db12a483ba4b8aff
Gabriely <gabriely.lima@nave.rs>
2023-01-24 22:59:52 -0300

    mobile: verifying is selected date is in past and showing disabled habits and message

--- mobile/src/screens/Habit/index.tsx
+++ mobile/src/screens/Habit/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import { View, ScrollView, Text, Alert } from "react-native"
 
 import dayjs from "dayjs"
+import clsx from "clsx"
 import { useRoute } from "@react-navigation/native"
 
 import { api } from "../../lib/axios"
@@ -34,6 +35,7 @@ export function Habit() {
   const { date } = route.params as HabitParams
 
   const parsedDate = dayjs(date)
+  const isDateInPast = parsedDate.endOf("day").isBefore(new Date())
   const dayOfWeek = parsedDate.format("dddd")
   const dayAndMonth = parsedDate.format("DD/MM")
 
@@ -97,13 +99,14 @@ export function Habit() {
 
         <ProgressBar progress={habitsProgress} />
 
-        <View className="mt-6">
+        <View className={clsx("mt-6", { ["opacity-50"]: isDateInPast })}>
           {dayInfo?.possibleHabits ? (
             dayInfo?.possibleHabits.map((habit) => (
               <Checkbox
                 key={habit.id}
                 title={habit.title}
                 checked={completedHabits.includes(habit.id)}
+                disabled={isDateInPast}
                 onPress={() => handleToggleHabit(habit.id)}
               />
             ))
@@ -111,6 +114,12 @@ export function Habit() {
             <HabitsEmpty />
           )}
         </View>
+
+        {isDateInPast && (
+          <Text className="text-white mt-10 text-center">
+            Você não pode editar hábitos de uma data passada.
+          </Text>
+        )}
       </ScrollView>
     </View>
   )
