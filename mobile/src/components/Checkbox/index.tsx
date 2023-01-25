aa20623f6de2363e8878bd28fe420a75036951ca
Gabriely <gabriely.lima@nave.rs>
2023-01-25 18:05:33 -0300

    mobile: adding animations to ProgressBar and Checkbox components

--- mobile/src/components/Checkbox/index.tsx
+++ mobile/src/components/Checkbox/index.tsx
@@ -7,6 +7,7 @@ import {
 import { Feather } from "@expo/vector-icons"
 
 import colors from "tailwindcss/colors"
+import Animated, { ZoomIn, ZoomOut } from "react-native-reanimated"
 
 interface CheckboxProps extends TouchableOpacityProps {
   checked?: boolean
@@ -17,9 +18,13 @@ export function Checkbox({ checked = false, title, ...rest }: CheckboxProps) {
   return (
     <TouchableOpacity className="flex-row m-2 items-center" {...rest}>
       {checked ? (
-        <View className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center">
+        <Animated.View
+          className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center"
+          entering={ZoomIn}
+          exiting={ZoomOut}
+        >
           <Feather name="check" size={20} color={colors.white} />
-        </View>
+        </Animated.View>
       ) : (
         <View className="h-8 w-8 bg-zinc-900 rounded-lg"></View>
       )}
--- mobile/src/components/ProgressBar/index.tsx
+++ mobile/src/components/ProgressBar/index.tsx
@@ -3,18 +3,33 @@ import { useRoute } from "@react-navigation/native"
 import { BackButton } from "../../components/BackButton"
 
 import dayjs from "dayjs"
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated"
+import { useEffect } from "react"
 
 interface ProgressBarProps {
   progress?: number
 }
 
 export function ProgressBar({ progress = 0 }: ProgressBarProps) {
+  const sharedProgress = useSharedValue(progress)
+
+  const style = useAnimatedStyle(() => {
+    return {
+      width: `${sharedProgress.value}%`,
+    }
+  })
+
+  useEffect(() => {
+    sharedProgress.value = withTiming(progress)
+  }, [progress])
+
   return (
     <View className="w-full h-3 rounded-xl bg-zinc-700 mt-4">
-      <View
-        className="h-3 rounded-xl bg-violet-600"
-        style={{ width: `${progress}%` }}
-      />
+      <Animated.View className="h-3 rounded-xl bg-violet-600" style={style} />
     </View>
   )
 }
